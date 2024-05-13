3e78a900159d8028bdb06fdf1b3e91a69dd0a22c
Paakow Wobil <pwobil59@gmail.com>
2024-05-13 17:04:20 -0300

    Deployment of the database

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -3,6 +3,7 @@ import api from "../api"
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css"
+import LoadingIndicator from "./LoadingIndicator";
 
 function Form ({route, method}){
     const [username, setUsername] = useState("")
@@ -53,6 +54,7 @@ function Form ({route, method}){
             onChange={(e) => setPassword(e.target.value)} //set password to value "e" that is typed here
             placeholder="Password"
         />
+        {loading && <LoadingIndicator />}
         <button className="form-button" type="submit">
             {name}
         </button>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../api";
+import Note from "../components/Notes"
+import "../styles/Home.css"
 
 function Home() {
     const [notes, setNotes] = useState([]);
@@ -23,22 +25,25 @@ function Home() {
         api.delete(`api/notes/delete/${id}/`).then((res) => {
             if (res.status ===204) alert("Note deleted!");
             else alert("Failed to delete note.");
+            getNotes();
         }).catch((error) => alert(error));
-        getNotes();
+        
     };
 
     const createNote = (e) => {
         e.preventDefault()
         api.post("api/notes/", {content, title}).then((res) => {
             if(res.status ===201) alert("Note created!");
-            else alert("Failed to make note.")
+            else alert("Failed to make note.");
+            getNotes(); 
         }).catch((err) => alert(err));
-        getNotes(); 
+        
     }
 
     return <div>
         <div>
             <h2>Notes</h2>
+            {notes.map((note) => (<Note note={note} onDelete={deleteNote} key={note.id}/>))}
         </div>
 
         <h2>Create a Note</h2>
