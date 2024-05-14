0d6f395fda62a30d4c9b3f872b287b7be7525bd7
Paakow Wobil <pwobil59@gmail.com>
2024-05-14 00:27:19 -0300

    fix to deployment issues

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -12,16 +12,16 @@ const apiUrl = "/choreo-apis/notetracker/backend/rest-api-be2/v1"
 
 //
 const api = axios.create({
-    baseURL: apiUrl,
-})
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  });
 
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem(ACCESS_TOKEN);
         if (token){
-            config.headers.Authorization = `Bearer ${token}`
+            config.headers.Authorization = `Bearer ${token}`;
         }
-        return config
+        return config;
     },
 
     (error) => {
